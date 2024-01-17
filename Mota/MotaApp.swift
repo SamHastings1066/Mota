42750337fd8f469028a20ce5f5be3187f50d4aa1
Sam Hastings <samhastingsis@gmail.com>
2024-01-17 10:49:03 -0500

    Initial Commit

---
+++ Mota/MotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotaApp.swift
+//  Mota
+//
+//  Created by sam hastings on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
