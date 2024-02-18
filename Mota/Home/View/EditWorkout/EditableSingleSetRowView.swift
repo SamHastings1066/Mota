2e9acb7b207f8ab8f189a3de0cdf5ce640c2e0bb
Sam Hastings <samhastingsis@gmail.com>
2024-02-18 18:55:09 -0500

    Add consistentWeights and consistentReps to SuperSet model and to front end views.

---
+++ Mota/Home/View/EditWorkout/EditableSingleSetRowView.swift
@@ -0,0 +1,18 @@
+//
+//  EditableSingleSetRowView.swift
+//  Mota
+//
+//  Created by sam hastings on 18/02/2024.
+//
+
+import SwiftUI
+
+struct EditableSingleSetRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditableSingleSetRowView()
+}
