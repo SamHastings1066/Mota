5012b29cb9d64d586fa72ce59ee7424ff9536e95
Sam Hastings <samhastingsis@gmail.com>
2024-07-25 11:31:38 -0400

    Add generic AsyncPreviewView for use in previews which require async setup.

---
+++ Mota/Preview Content/AsyncPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncPreviewView.swift
+//  Mota
+//
+//  Created by sam hastings on 25/07/2024.
+//
+
+import SwiftUI
+
+struct AsyncPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncPreviewView()
+}
